83a90a719a96c6e949f59d0e40a49cd9928b6f78
Erick Bejarano <erick.935@gmail.com>
2018-10-22 21:01:40 -0500

    se termina el componente de fsm

--- src/app/components/fsm/fsm.component.ts
+++ src/app/components/fsm/fsm.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { FSM } from 'src/app/model/fsm/fsm';
 
 @Component({
-  selector: 'app-fsm',
-  templateUrl: './fsm.component.html',
-  styleUrls: ['./fsm.component.css']
+    selector: 'app-fsm',
+    templateUrl: './fsm.component.html',
+    styleUrls: ['./fsm.component.css']
 })
 export class FsmComponent implements OnInit {
 
-  constructor() { }
+    public fsm: FSM;
 
-  ngOnInit() {
-  }
+    constructor() {
+        this.fsm = new FSM();
+    }
 
+    ngOnInit() {}
+
+    public calcularResultadoFinal(derecho, izquierdo) {
+        return (derecho < izquierdo ? derecho : izquierdo );
+    }
+
+    public calcularResultadosFinales() {
+        this.fsm.desplanteF = this.calcularResultadoFinal(this.fsm.desplanteD, this.fsm.desplanteI);
+        this.fsm.estabilidadF = this.calcularResultadoFinal(this.fsm.estabilidadD, this.fsm.estabilidadI);
+        this.fsm.levantamientoF = this.calcularResultadoFinal(this.fsm.levantamientoD, this.fsm.levantamientoI);
+        this.fsm.movilidadF = this.calcularResultadoFinal(this.fsm.movilidadD, this.fsm.movilidadI);
+        this.fsm.pasoObsF = this.calcularResultadoFinal(this.fsm.pasoObsD, this.fsm.pasoObsI);
+        this.calcularTotales();
+    }
+
+    public calcularTotales() {
+        this.fsm.totales = this.fsm.pasoObsF + this.fsm.desplanteF + this.fsm.movilidadF +
+            this.fsm.levantamientoF + this.fsm.estabilidadF +
+            this.fsm.sentadilla + this.fsm.estabilidadT;
+    }
 }
---
+++ src/app/model/fsm/fsm.ts
@@ -0,0 +1,57 @@
+export class FSM {
+
+    public pasoObsD: number;
+    public pasoObsI: number;
+    public pasoObsF: number;
+    public pasoObs: string;
+    public desplanteD: number;
+    public desplanteI: number;
+    public desplanteF: number;
+    public desplanteObs: string;
+    public movilidadD: number;
+    public movilidadI: number;
+    public movilidadF: number;
+    public movilidadObs: string;
+    public levantamientoD: number;
+    public levantamientoI: number;
+    public levantamientoF: number;
+    public levantamientoObs: string;
+    public estabilidadD: number;
+    public estabilidadI: number;
+    public estabilidadF: number;
+    public estabilidadObs: string;
+
+    public sentadilla: number;
+    public sentadillaObs: string;
+    public estabilidadT: number;
+    public estabilidadTObs: string;
+    public totales: number;
+
+
+    constructor() {
+        this.pasoObsD = 0;
+        this.pasoObsI = 0;
+        this.pasoObsF = 0;
+        this.pasoObs = '';
+        this.desplanteD = 0;
+        this.desplanteI = 0;
+        this.desplanteF = 0;
+        this.desplanteObs = '';
+        this.movilidadD = 0;
+        this.movilidadI = 0;
+        this.movilidadF = 0;
+        this.movilidadObs = '';
+        this.levantamientoD = 0;
+        this.levantamientoI = 0;
+        this.levantamientoF = 0;
+        this.levantamientoObs = '';
+        this.estabilidadD = 0;
+        this.estabilidadI = 0;
+        this.estabilidadF = 0;
+        this.estabilidadObs = '';
+        this.sentadilla = 0;
+        this.sentadillaObs = '';
+        this.estabilidadT = 0;
+        this.estabilidadTObs = '';
+    }
+}
